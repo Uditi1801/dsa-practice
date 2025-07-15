cb6df4cbebca6532d459f30cea71514e031379ca
Uditi1801 <uditi18012008@gmail.com>
2025-07-15 17:15:57 +0530

    Create find the first non repeating character in string.py

---
+++ Strings/find the first non repeating character in string.py
@@ -0,0 +1,9 @@
+#find the first non repeating character in string
+def first_non_repeating_char(s):
+    from collections import Counter
+    count=Counter(s)
+    for char in s:
+        if count[char]==1:
+            return char
+    return None
+print(first_non_repeating_char("aabbcde"))
