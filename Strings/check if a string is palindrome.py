1f7d30e936548d3d036da9f3ebd859411777210f
Uditi1801 <uditi18012008@gmail.com>
2025-07-25 17:44:01 +0530

    Update check if a string is palindrome.py

--- Strings/check if a string is palindrome.py
+++ Strings/check if a string is palindrome.py
@@ -1,4 +1,20 @@
 #check if a string is palindrome
+#Method1:
 def is_palindrome(s):
     return s==s[::-1]
 print(is_palindrome("listen"))
+
+#Method2:
+def is_palindrome(s):
+    left=0
+    right=len(s)-1
+    while left<right:
+        while left<right and not s[left].isalnum():
+            left+=1
+        while left<right and not s[right].isalnum():
+            right-=1
+        if s[left].lower()!=s[right].lower():
+            return False
+        left+=1
+        right-=1
+    return True
