8619e6f6490093c90cbc32efd6cacbdc7d438976
Uditi1801 <uditi18012008@gmail.com>
2025-07-25 17:34:06 +0530

    Update reversing a string.py

--- Strings/reversing a string.py
+++ Strings/reversing a string.py
@@ -1,4 +1,14 @@
 #reversing a string
+#Method1:
 def reverse_string(s):
     return s[::-1]
 print(reverse_string("hello"))
+
+#Method2:
+def reversed_string(s):
+    result=""
+    for char in s:
+        result=char+result#adds current char to front
+    return result
+print(reversed_string("hello"))
+print(reversed_string("uditi"))
