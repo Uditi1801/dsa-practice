93b336b4f05658333fb4d130be8b340df8d76a56
Uditi1801 <uditi18012008@gmail.com>
2026-01-30 01:43:50 +0530

    Implement array element printing in new lines
    Print each element of the array on a new line.

---
+++ Arrays/printing each element of array in new line.cpp
@@ -0,0 +1,13 @@
+/*[3, 7, 1, 9, 4] print each element
+on a new line*/
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={3,7,1,9,4};
+    int n=5;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+
+}
